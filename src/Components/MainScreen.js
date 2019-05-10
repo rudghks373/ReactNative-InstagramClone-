2dc6d8803038550bde091d3b10936c84b18f26b3
오경환 <32234263+rudghks373@users.noreply.github.com>
2019-05-10 15:10:39 +0900

    Updata Setting

---
+++ src/Components/MainScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { StyleSheet, Platform } from "react-native";
+
+class MainScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>MainScreen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default MainScreen;
