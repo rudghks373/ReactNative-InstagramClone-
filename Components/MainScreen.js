93d3e0a2f246447b2926d66a9980e2efc67f1af1
rudghks373 <rudghks373@naver.com>
2019-07-04 21:15:30 +0900

    Updata

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Icon } from 'native-base'; // 추가된 코드
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'; 
+
+//하단 탭에 들어갈 컴포넌트
+import HomeTab from './AppTabNavigator/HomeTab'
+import SearchTab from './AppTabNavigator/SearchTab'
+import AddMediaTab from './AppTabNavigator/AddMediaTab'
+import LikesTab from './AppTabNavigator/LikesTab'
+import ProfileTab from './AppTabNavigator/ProfileTab'
+
+const AppTabNavigator = createBottomTabNavigator ({
+HomeTab: {screen: HomeTab},
+SearchTab: {screen: SearchTab},
+AddMediaTab: {screen: AddMediaTab},
+LikesTab: {screen: LikesTab},
+ProfileTab: {screen: ProfileTab}
+});
+
+const AppTabContainet = createAppContainer(AppTabNavigator);
 
 export default class MainScreen extends Component {
 
@@ -13,10 +31,7 @@ export default class MainScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>   Kyunghwan  </Text>
-        <Text>♡〜٩( ˃́▿˂̀ )۶〜♡</Text>
-      </View>
+        <AppTabContainet/>
     );
   }
 }
