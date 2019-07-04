e46868d88f3ff044101cc9c6d1ca5c46af6d204c
rudghks373 <rudghks373@naver.com>
2019-07-04 21:43:32 +0900

    Feed function add

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { Icon } from 'native-base';
+import { Container, Content, Icon} from 'native-base';
+import CardComponent from '../CardComponent'; // 카드 컴포넌트 추가
 
 export default class HomeTab extends Component {
+
+    fetchFeeds() {
+        const data = {
+            id: 1,
+            jsonrpc: "2.0",
+            method: "call",
+            params: [
+              "database_api",
+              "get_discussions_by_created",
+              [{ tag: "kr", limit: 20 }]
+            ]
+        };
+        return fetch('https://api.steemit.com', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(res => res.result)
+    }
 
     static navigationOptions = {
         tabBarIcon: ({ tintColor }) => (
@@ -10,11 +30,27 @@ export default class HomeTab extends Component {
         )
     }
 
+    state = {
+        feeds: []
+    }
+
+    componentWillMount() {
+        this.fetchFeeds().then(feeds => {
+            this.setState({
+              feeds
+            })
+        });
+    }
+
     render() {
         return (
-            <View style={style.container}>
-                <Text>HomeTab</Text>
-            </View>
+            <Container style={style.container}>
+                <Content>
+                {
+            this.state.feeds.map(feed => <CardComponent data={ feed }/>)
+                }
+                </Content>
+            </Container>
         );
     }
 }
@@ -22,7 +58,6 @@ export default class HomeTab extends Component {
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: 'white'
     }
 });
